352d665f232b03f51785ce100d4000072094e9d9
Axcee Cabusas <115922404+sumkai16@users.noreply.github.com>
2025-02-19 16:51:12 +0800

    db updated

--- prospectus.sql
+++ prospectus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 16, 2025 at 02:17 PM
+-- Generation Time: Feb 19, 2025 at 09:50 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,65 @@ SET time_zone = "+00:00";
 --
 -- Database: `prospectus`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `c_id` int(11) NOT NULL,
+  `c_code` int(11) NOT NULL,
+  `c_desc` int(11) NOT NULL,
+  `c_units` int(11) NOT NULL,
+  `c_prerequisites` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `program`
+--
+
+CREATE TABLE `program` (
+  `p_id` int(11) NOT NULL,
+  `p_program_name` varchar(255) NOT NULL,
+  `p_desc` varchar(255) NOT NULL,
+  `p_department` varchar(255) NOT NULL,
+  `p_status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prospectus`
+--
+
+CREATE TABLE `prospectus` (
+  `pr_id` int(11) NOT NULL,
+  `program_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `pr_effective_year` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `s_id` int(11) NOT NULL,
+  `s_fname` varchar(255) NOT NULL,
+  `s_mname` varchar(255) NOT NULL,
+  `s_lname` varchar(255) NOT NULL,
+  `s_bdate` date NOT NULL,
+  `s_address` text NOT NULL,
+  `s_year` int(11) NOT NULL,
+  `s_program` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -51,6 +110,32 @@ INSERT INTO `user` (`u_id`, `u_fname`, `u_mname`, `u_lname`, `u_email`, `u_conta
 --
 
 --
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`c_id`);
+
+--
+-- Indexes for table `program`
+--
+ALTER TABLE `program`
+  ADD PRIMARY KEY (`p_id`);
+
+--
+-- Indexes for table `prospectus`
+--
+ALTER TABLE `prospectus`
+  ADD PRIMARY KEY (`pr_id`),
+  ADD KEY `program_id` (`program_id`),
+  ADD KEY `course_id` (`course_id`) USING BTREE;
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`s_id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -61,10 +146,45 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `course`
+--
+ALTER TABLE `course`
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `program`
+--
+ALTER TABLE `program`
+  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `prospectus`
+--
+ALTER TABLE `prospectus`
+  MODIFY `pr_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `s_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `prospectus`
+--
+ALTER TABLE `prospectus`
+  ADD CONSTRAINT `prospectus_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `course` (`c_id`),
+  ADD CONSTRAINT `prospectus_ibfk_2` FOREIGN KEY (`program_id`) REFERENCES `program` (`p_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
